1a320e545cbf5e7de5c7f9d8c3a3405bb32153d0
Fatih Emin KARAHAN <fatih81443@gmail.com>
2023-09-02 17:07:21 +0300

    Quiz App

---
+++ QuizApp/app/src/main/java/com/fatiheminkarahan/quizapp/QuestionAnswer.java
@@ -0,0 +1,27 @@
+package com.fatiheminkarahan.quizapp;
+
+public class QuestionAnswer {
+    public static String question[] ={
+            "Which company owns the android?",
+            "Which one is not the programming language?",
+            "Which country has the most people?",
+            "Which planet is the fifth from the sun?",
+            "In which country is Mount Everest?"
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"India","Türkiye","China","Germany"},
+            {"Mars","Uranus","Jupiter","Venus"},
+            {"France","Sweden","Mexico","Nepal"}
+    };
+
+    public static String correctAnswers[] = {
+            "Google",
+            "Notepad",
+            "India",
+            "Jupiter",
+            "Nepal"
+    };
+}
